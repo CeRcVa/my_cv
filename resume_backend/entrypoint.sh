812fd738a85ff221de0f062ffc45b5a59ed3e410
CeRcVa <shotikogamer16@gmail.com>
2026-02-17 22:47:34 +0400

    update
    shell

--- resume_backend/entrypoint.sh
+++ resume_backend/entrypoint.sh
@@ -16,3 +16,20 @@ gunicorn resume_backend.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers 2 \
   --timeout 60
+
+echo "Creating superuser if not exists..."
+python manage.py shell <<'EOF'
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created")
+else:
+    print("Superuser exists or env vars missing")
+EOF
