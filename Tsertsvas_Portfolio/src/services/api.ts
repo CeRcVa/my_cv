912cbdcdc4bbe1528c08e84218cba98e326ba74e
CeRcVa <shotikogamer16@gmail.com>
2026-02-18 17:41:43 +0400

    latest

--- Tsertsvas_Portfolio/src/services/api.ts
+++ Tsertsvas_Portfolio/src/services/api.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 import.meta.env.VITE_API_BASE
 
-console.log("VITE_API_BASE =", import.meta.env.VITE_API_BASE);
 export const API_BASE = import.meta.env.VITE_API_BASE ?? "";
-console.log("ENV CHECK:", import.meta.env.VITE_API_BASE);
-
 
 const api = axios.create({
   baseURL: `${API_BASE}/api/`,
