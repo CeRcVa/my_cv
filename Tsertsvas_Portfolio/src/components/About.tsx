1b3c5271776dd52a3ebbb4a765592d58b09188f6
CeRcVa <shotikogamer16@gmail.com>
2026-02-18 18:52:29 +0400

    photo

--- Tsertsvas_Portfolio/src/components/About.tsx
+++ Tsertsvas_Portfolio/src/components/About.tsx
@@ -29,9 +29,12 @@ export default function About({ profile }: { profile: Profile | null }) {
         </div>
 
         <div className="avatarWrap">
-          <div className="avatarInner">
-            <img src={profileImg} alt="Profile" className="avatarImg" />
-          </div>
+          <img
+            src={profileImg}
+            alt={profile?.name ?? "Profile"}
+            className="avatarImg"
+            loading="lazy"
+          />
         </div>
       </div>
     </section>
