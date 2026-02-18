3da5c075583bcae9fb47bcebb64e446c4bb7a762
CeRcVa <shotikogamer16@gmail.com>
2026-02-18 18:18:38 +0400

    png

--- Tsertsvas_Portfolio/src/components/Projects.tsx
+++ Tsertsvas_Portfolio/src/components/Projects.tsx
@@ -2,8 +2,8 @@ import type { Project } from "../types";
 import { resolveMediaUrl } from "../services/api";
 
 import p1 from "../assets/projects/project1.png";
-import p2 from "../assets/projects/project2.png";
-import p3 from "../assets/projects/project3.png";
+import p2 from "../assets/projects/project2.PNG";
+import p3 from "../assets/projects/project3.PNG";
 
 // 🔥 აქ ჩაწერე ზუსტად ის სახელები, რაც Admin-იდან მოდის (p.name)
 const projectImages: Record<string, string> = {
